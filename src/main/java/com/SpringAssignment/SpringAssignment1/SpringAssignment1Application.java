3281390a1aec3d2b21943c191c32d4fe1d45f9b8
Hinal <hinal.p@simformsolutions.com>
2023-06-06 17:51:26 +0530

    Spring Assignment 1 Added

---
+++ src/main/java/com/SpringAssignment/SpringAssignment1/SpringAssignment1Application.java
@@ -0,0 +1,14 @@
+package com.SpringAssignment.SpringAssignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignment1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Application run Successfully");
+		SpringApplication.run(SpringAssignment1Application.class, args);
+	}
+
+}
